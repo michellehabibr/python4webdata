11e8991dec1f45d0c3d5ad86d70e54a38c61d961
michellehabibr <a00824353@itesm.mx>
2022-10-11 17:28:20 -0500

    Update and rename extracting_data_from_xml to extracting_data_from_xml.py

--- extracting_data_from_xml.py
+++ extracting_data_from_xml.py
@@ -1,3 +1,4 @@
+"""
 In this assignment you will write a Python program somewhat similar to http://www.py4e.com/code3/geoxml.py. The program will prompt for a URL, read the XML data from that URL using urllib and then parse and extract the comment counts from the XML data, compute the sum of the numbers in the file.
 
 We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
@@ -18,8 +19,9 @@ Retrieving http://py4e-data.dr-chuck.net/comments_42.xml
 Retrieved 4189 characters
 Count: 50
 Sum: 2...
-
-CODE:
+"""
+# CODE:
+#Michelle Habib Oct-11-2022
 import urllib.request, urllib.parse, urllib.error
 import xml.etree.ElementTree as ET
 import ssl
